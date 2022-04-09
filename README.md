9dd435af318d30da8d6dde392cc376bd2ccfce89
youssif-sayed <spiderboyyoussif@yahoo.com>
2022-04-10 00:51:50 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# e_wallet
+ Graduation Project
---
+++ android/app/src/main/kotlin/com/yousefsayed/e_wallet/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.yousefsayed.e_wallet
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
